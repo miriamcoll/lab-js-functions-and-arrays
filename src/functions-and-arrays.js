faf1feadb4ea4d8d0014f5f7252d9ff880dd424f
Miriam Collado <miriamcollado.a@gmail.com>
2024-10-04 17:01:25 +0200

    solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,29 +1,62 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(n1, n2) {
+    if (n1 > n2) {
+    return n1;
+}
+    else {
+    return n2;
+}
+}
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(arrayOfWords) {
+    if (arrayOfWords.lenght === 0) {
+        return null;
+    }
+    let longestWord = arrayOfWords[0];
+    for (let i = 0; i < arrayOfWords.lenght; i++) {
+        if (arrayOfWords[i].lenght > longestWord.length) {
+            longestWord = arrayOfWords[i];
+        }
+    }
+    return longestWord;
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++ ) {
+      sum += numbers[i];
+    }
+    return sum
+    }
+    console.log(sumNumbers(numbers))
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+    }
+
+    let totalSum = sumNumbers(numbers2);
+
+    const average = totalSum / numbers2.length;
+    return average;
+}
+
+console.log(averageNumbers(numbers2))
 
 
 
@@ -31,4 +64,21 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordsArray, wordToFind) {
+    if (wordsArray.length === 0) {
+        return null;
+    }
+
+    for (let i = 0; i < wordsArray.length; i++) {
+        if (wordsArray[i] === wordToFind) {
+            return true; 
+        }
+      else {
+         return false; 
+      }
+    }
+}
+
+console.log(doesWordExist(words2, "machine"))
+
+
